7f709d93d604300915466c2ed760ba37db0c3aa0
Titas Milan <titas9x@gmail.com>
2011-12-27 15:57:27 +0530

    Ran the cucumber and rspec generators

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+
+
+gem 'sqlite3'
+
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+
+group :test, :development do
+  gem 'rspec-rails', '~> 2.5'
+end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticktee::Application.config.secret_token = '728cff9c75f7ca7a91087c63e8bf6953b0f8a86a4a6f240dcd303c3f0917de28035e6f6071fde51a08175e8bd577544a0b87539e64216ff28070aeecbf086cf3'
